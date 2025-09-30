d2fd287d5ccdf7a94883c3890ced1d5021a189d7
Przemas <przemyslvw@gmail.com>
2025-09-30 23:04:21 +0200

    akwarele  w galerii

--- js/gallery.js
+++ js/gallery.js
@@ -9,7 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         'gallery/2025-09-28_16-51.png',
         'gallery/2025-09-28 at 16.49.30.jpg',
         'gallery/Cuadro foto original.jpg',
-        'gallery/2025-09-28 at 16.49.30.jpg',
+        // Akwarele collection
+        'gallery/akwarele/70n2x970n2x970n2.png',
+        'gallery/akwarele/Image_1himu01himu01him.png',
+        'gallery/akwarele/Image_1tewa51tewa51tew.png',
+        'gallery/akwarele/Image_7062q67062q67062.png',
+        'gallery/akwarele/Image_sxrf2sexrf2sexr.png',
+        'gallery/akwarele/afq57fafq57fafq5.png',
+        'gallery/akwarele/cb82v3cb82v3cb82.png',
+        'gallery/akwarele/oh19duoh19duoh19.png',
+        'gallery/akwarele/qv9ap3qv9ap3qv9a.png',
+        'gallery/akwarele/tjdoyotjdoyotjdo.png',
+        'gallery/akwarele/ve1x4jve1x4jve1x.png',
+        'gallery/akwarele/w6kl73w6kl73w6kl.png',
+        'gallery/akwarele/y0ewupy0ewupy0ew.png'
     ];
 
     // Clear existing static content
